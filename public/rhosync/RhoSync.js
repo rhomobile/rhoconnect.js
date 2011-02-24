b47555a72af1642fc0ea81af0d053377e9f56a90
Dmitry Prokhorov <dipspb@gmail.com>
2011-02-24 15:42:31 +0300

    Login inplemented

--- public/rhosync/RhoSync.js
+++ public/rhosync/RhoSync.js
@@ -1,37 +1,100 @@
 (function($) {
-    $.RhoSync = function(config) {
-		var defaults = {
+    RhoSync = function(cfg) {
+
+        const SESSION_COOKIE = 'rhosync_session';
+        const NOTIFY_GENERIC = 'rhoSyncNotifyGeneric';
+
+        var defaults = {
 			syncserver: "",
 			pollinterval: 20
-		}
-		var config = $.extend({}, defaults, config);
+		};
 
-		function _net_call(url,data) {
-		    var dfr = $.Deferred();
+		var config = $.extend({}, defaults, cfg);
+
+        function _setCookie(name, value, days, path, domain, secure) {
+            if (days) {
+                var expDate = new Date();
+                expDate.setTime(expDate.getTime() + (days * 24 * 60 * 60 * 1000));
+            }
+            document.cookie = name + "=" + encodeURIComponent( value ) +
+                ((days) ? "; expires=" + expDate.toGMTString() : "") +
+                ((path) ? "; path=" + path : "") +
+                ((domain) ? "; domain=" + domain : "") +
+                ((secure) ? "; secure" : "");
+        }
+
+        function _getCookie(name) {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].split('=');
+                var cName = cookie[0].replace(/^\s+|\s+$/g, ''); // trim side spaces
+                if (cName == name) {
+                    return (1 < cookie.length) ? cookie[1].replace(/^\s+|\s+$/g, '') : ''; // trim side spaces
+                }
+            }
+            return null;
+        }
+
+        function _deleteCookie(name, path, domain) {
+            if (_getCookie(name)) {
+                document.cookie = name + "=" +
+                    ((path) ? "; path=" + path : "") +
+                    ((domain) ? "; domain=" + domain : "") +
+                    "; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+            }
+        }
+
+        function _notifyHandler(status, data, xhr) {
+            $(window).trigger(jQuery.Event(NOTIFY_GENERIC), [{status: status, data: data}]);
+            // fire exact notifications here
+        }
+
+        function _net_call(url,data) {
+            var dfr = $.Deferred();
 		    $.ajax({
 		    	url: url,
-		     	data: data,
-		     	dataType: 'jsonp',
-		     	success: dfr.resolve,
-				error: function (xhr, status) {
-					dfr.reject(xhr.responseText);
-				}
-		    });
-		    return dfr.promise();
+                type: 'post',
+                contentType: 'application/json',
+                processData: false,
+		     	data: $.toJSON(data),
+                dataType: 'json'
+		    }).done(function(data, status, xhr){
+                _notifyHandler(status, data, xhr);
+                dfr.resolve(status, data, xhr);
+            }).fail(function(xhr, status, error){
+                _notifyHandler(status, error, xhr);
+                dfr.reject(status, error, xhr);
+            });
+            return dfr.promise();
 		}
 			
-		function login(login,password,callback) {
-			_net_call('/login',{login:login,password:password}
-			).done(function(data) {
-				callback("sucess",data);
-			}).fail(function(data) {
-				callback("error",data);
-			});
-		}
+        function login(login, password) {
+            var dfr = $.Deferred();
+            _net_call(config.syncserver+'/clientlogin', {login:login, password:password, rememberme: 1}
+            ).done(function(status, data, xhr) {
+                dfr.resolve(data, xhr.responseText);
+            }).fail(function(status, error, xhr) {
+                dfr.reject(error, xhr.responseText);
+            });
+            return dfr.promise();
+        }
 				
+        function isLoggedIn() {
+            return _getCookie(SESSION_COOKIE) ? true : false;
+        }
+
+        function logout() {
+            _deleteCookie(SESSION_COOKIE);
+        }
+
 		return {
 			api: {
-				login:login
+                events: {
+                    NOTIFY_GENERIC: NOTIFY_GENERIC
+                },
+                login: login,
+                isLoggedIn: isLoggedIn,
+                logout: logout
 			},
 			rhoconfig: config
 		}
--- spec/javascripts/RhoSyncSpec.js
+++ spec/javascripts/RhoSyncSpec.js
@@ -1,27 +1,62 @@
 describe("RhoSync", function() {
+//    var syncUrl = "http://rhodes-store-server.heroku.com/application";
+    var syncUrl = "http://localhost:9292/application";
+
+
 	beforeEach(function() {
-		rhosync = $.RhoSync({syncserver:"http://localhost:9292/application"})
+		rhosync = RhoSync({syncserver:syncUrl});
+        notified = false;
+        notify = function(evt, obj){
+            jasmine.log(evt.type + ': ' + $.toJSON(obj));
+            notified = true;
+        };
 	});
   
 	it("should be initialized", function() {
-    	expect(rhosync.rhoconfig.syncserver).toEqual("http://localhost:9292/application");
+    	expect(rhosync.rhoconfig.syncserver).toEqual(syncUrl);
  	});
 
-	it("should login", function() {
-	    fakeAjax({urls: {'/login': {successData: 'login'}}});
-		var result = 'blah';
-		rhosync.api.login("user","password",function(status,data) {
-			result = data;
-		});
-	    expect(result).toEqual('login');
-	});
+    it("should login ok with proper credentials", function() {
+        //fakeAjax({urls: {'/login': {successData: 'login'}}});
 
-	it("should fail to login", function() {
-	    fakeAjax({urls: {'/login': {errorMessage: 'Unknown user'}}});
-		var result = 'blah';
-		rhosync.api.login("user","password",function(status,data) {
-			result = data;
-		});
-	    expect(result).toEqual('Unknown user');
-	});
+        var hdlr = jasmine.createSpy('ajax handler spy');
+
+        $(window).bind(rhosync.api.events.NOTIFY_GENERIC, null, notify);
+
+        rhosync.api.login("lars", "larspass").done(function(data, response){
+            hdlr("success", response);
+        }).fail(function(data, response){
+            hdlr("error", response);
+        });
+
+        waitsFor(function(){ return 0 < hdlr.callCount;}, 3000);
+        runs(function(){
+            expect(hdlr).toHaveBeenCalledWith("success", "");
+            expect(notified).toBeTruthy();
+
+            $(window).unbind(rhosync.api.events.NOTIFY_GENERIC, notify);
+        });
+    });
+
+    it("should fail to login with wrong credentials", function() {
+        //fakeAjax({urls: {'/login': {successData: 'error'}}});
+
+        var hdlr = jasmine.createSpy('ajax handler spy');
+
+        $(window).bind(rhosync.api.events.NOTIFY_GENERIC, null, notify);
+
+        rhosync.api.login("not_lars", "not_larspass").done(function(data, response){
+            hdlr("success", response);
+        }).fail(function(data, response){
+            hdlr("error", response);
+        });
+
+        waitsFor(function(){ return 0 < hdlr.callCount;}, 3000);
+        runs(function(){
+            expect(hdlr).toHaveBeenCalledWith("error", "");
+            expect(notified).toBeTruthy();
+
+            $(window).unbind(rhosync.api.events.NOTIFY_GENERIC, notify);
+        });
+    });
 });
