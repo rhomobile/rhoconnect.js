e8cd7ee159fd6079dd8ae308386e6aeac4719bec
Dmitry Prokhorov <dipspb@gmail.com>
2011-02-25 23:59:54 +0300

    clientcreate  request and storage with deferred api implemented

--- public/rhosync/RhoSync.js
+++ public/rhosync/RhoSync.js
@@ -2,7 +2,9 @@
     RhoSync = function(cfg) {
 
         const SESSION_COOKIE = 'rhosync_session';
+
         const NOTIFY_GENERIC = 'rhoSyncNotifyGeneric';
+        const NOTIFY_CLIENT_CREATED = 'rhoSyncNotifyClientCreated';
 
         var defaults = {
 			syncserver: "",
@@ -10,6 +12,200 @@
 		};
 
 		var config = $.extend({}, defaults, cfg);
+
+        var initDbSchemaSQL = ''
+            +'CREATE TABLE client_info ('
+                    +' "client_id" VARCHAR(255) default NULL,'
+                    +' "session" VARCHAR(255) default NULL,'
+                    +' "token" VARCHAR(255) default NULL,'
+                    +' "token_sent" BIGINT default 0,'
+                    +' "reset" BIGINT default 0,'
+                    +' "port" VARCHAR(10) default NULL,'
+                    +' "last_sync_success" VARCHAR(100) default NULL);'
+            +'CREATE TABLE object_values ('
+                    +' "source_id" BIGINT default NULL,'
+                    +' "attrib" varchar(255) default NULL,'
+                    +' "object" varchar(255) default NULL,'
+                    +' "value" varchar default NULL);'
+            +'CREATE TABLE changed_values ('
+                    +' "source_id" BIGINT default NULL,'
+                    +' "attrib" varchar(255) default NULL,'
+                    +' "object" varchar(255) default NULL,'
+                    +' "value" varchar default NULL,'
+                    +' "attrib_type" varchar(255) default NULL,'
+                    +' "update_type" varchar(255) default NULL,'
+                    +' "sent" BIGINT default 0);'
+            +'CREATE TABLE sources ('
+                    +' "source_id" BIGINT PRIMARY KEY,'
+                    +' "name" VARCHAR(255) default NULL,'
+                    +' "token" BIGINT default NULL,'
+                    +' "sync_priority" BIGINT,'
+                    +' "partition" VARCHAR(255),'
+                    +' "sync_type" VARCHAR(255),'
+                    +' "metadata" varchar default NULL,'
+                    +' "last_updated" BIGINT default 0,'
+                    +' "last_inserted_size" BIGINT default 0,'
+                    +' "last_deleted_size" BIGINT default 0,'
+                    +' "last_sync_duration" BIGINT default 0,'
+                    +' "last_sync_success" BIGINT default 0,'
+                    +' "backend_refresh_time" BIGINT default 0,'
+                    +' "source_attribs" varchar default NULL,'
+                    +' "schema" varchar default NULL,'
+                    +' "schema_version" varchar default NULL,'
+                    +' "associations" varchar default NULL,'
+                    +' "blob_attribs" varchar default NULL);'
+            +'CREATE INDEX by_src_id on object_values ("source_id");'
+            +'CREATE UNIQUE INDEX by_src_object ON object_values ("object", "attrib", "source_id");'
+            +'CREATE INDEX by_src_value ON object_values ("attrib", "source_id", "value");'
+            ;
+
+        var dropDbSchemaSQL = ''
+            +'DROP INDEX IF EXISTS by_src_value;'
+            +'DROP INDEX IF EXISTS by_src_object;'
+            +'DROP INDEX IF EXISTS by_src_id;'
+            +'DROP TABLE IF EXISTS sources;'
+            +'DROP TABLE IF EXISTS changed_values;'
+            +'DROP TABLE IF EXISTS object_values;'
+            +'DROP TABLE IF EXISTS client_info;'
+            ;
+
+        var storage = function(dbName) {
+
+            function _tx(db) {
+                var dfr = $.Deferred();
+                db.transaction(function (tx) {
+                    dfr.resolve(tx);
+                }, function(err){
+                    dfr.reject(err);
+                });
+                return dfr.promise();
+            }
+
+            function _execSql(tx, sql, values) {
+                var dfr = $.Deferred();
+                tx.executeSql(sql, values, function(tx, rs){
+                    dfr.resolve(tx, rs);
+                }, function(tx, err){
+                    dfr.reject(tx, err);
+                });
+                return dfr.promise();
+            }
+
+            function _execInTx(db, sql, values) {
+                var dfr = $.Deferred();
+                _tx(db).done(function(tx) {
+                    _execSql(tx, sql, values).done(function(tx, rs){
+                        dfr.resolve(tx, rs);
+                    }).fail(function(tx, err){
+                        dfr.reject(tx, err);
+                    });
+                }).fail(function(err){
+                    dfr.reject(db, err);
+                });
+                return dfr.promise();
+            }
+
+            function open()
+            {
+                var dfr = $.Deferred();
+                try {
+                    var db = openDatabase(dbName, '1.0', 'RhoSync database', 2 * 1024 * 1024);
+                    dfr.resolve(db);
+                } catch(ex) {
+                    dfr.reject(ex);
+                }
+                return dfr.promise();
+            }
+
+            function close()/*throws DBException*/{}
+
+            function executeSQL(sql, values)
+            {
+                var dfr = $.Deferred();
+                open().done(function(db){
+                    _execInTx(db, sql, values).done(function(tx, rs){
+                        dfr.resolve(tx, rs);
+                    }).fail(function(obj, err){
+                        dfr.reject(obj, err);
+                    });
+                }).fail(function(ex){
+                    dfr.reject(null, ex);
+                });
+                return dfr.promise();
+            }
+
+            function executeBatchSQL(sql)
+            {
+                var dfr = $.Deferred();
+                var statements = sql.split(";");
+                for(var i in statements) {
+                    var stmt = statements[i];
+                    if(stmt) { // means: it is defined and not null and not empty string
+                        executeSQL(statements[i], null).fail(function(obj, err){
+                            dfr.reject(obj, err);
+                        });
+                    }
+                }
+                dfr.resolve();
+                return dfr.promise();
+            }
+
+            function iniDb()
+            {
+                return executeBatchSQL(initDbSchemaSQL);
+            }
+
+            function getAllTableNames()
+            {
+                var dfr = $.Deferred();
+                executeSQL("SELECT name FROM sqlite_master WHERE type='table'", null).done(function(tx, rs){
+                    var tableNames = [];
+                    for(var i=0; i<rs.rows.length; i++) {
+                        tableNames.push(rs.rows.item(i)['name']);
+                    }
+                    dfr.resolve(tx, tableNames);
+                }).fail(function(obj, err){
+                    dfr.reject(obj, err);
+                });
+                return dfr.promise();
+            }
+
+            return {
+                open: open,
+                close: close,
+                executeSQL: executeSQL,
+                executeBatchSQL: executeBatchSQL,
+                initSchema: iniDb,
+                getAllTableNames: getAllTableNames
+            }
+        }('rhoSyncDb');
+
+        var engine = function(){
+            var client = null;
+
+            function _clientCreate() {
+                return _net_call(config.syncserver+'/clientcreate', "", "get", "text/plain");
+            }
+
+            function clientInfo() {
+                return $.extend({}, client);
+            }
+
+            function clientCreate() {
+                _clientCreate().done(function(status, data){
+                    client = data;
+                    _notify(NOTIFY_CLIENT_CREATED, clientInfo());
+                }).fail(function(status, error){
+                    client = null;
+                    _notify(NOTIFY_CLIENT_CREATED, error);
+                });
+            }
+
+            return {
+                clientInfo: clientInfo,
+                clientCreate: clientCreate
+            }
+        }();
 
         function _setCookie(name, value, days, path, domain, secure) {
             if (days) {
@@ -44,41 +240,35 @@
             }
         }
 
-        function _notifyHandler(status, data, xhr) {
-            $(window).trigger(jQuery.Event(NOTIFY_GENERIC), [{status: status, data: data}]);
+        function _notify(type /*, arg1, arg2, ... argN*/) {
+            $(window).trigger(jQuery.Event(type), $.makeArray(arguments).slice(1));
             // fire exact notifications here
         }
 
-        function _net_call(url,data) {
+        function _net_call(url, data, method /*='post'*/, contentType /*='application/json'*/) {
             var dfr = $.Deferred();
 		    $.ajax({
 		    	url: url,
-                type: 'post',
-                contentType: 'application/json',
+                type: method || 'post',
+                contentType: contentType || 'application/json',
                 processData: false,
 		     	data: $.toJSON(data),
                 dataType: 'json'
 		    }).done(function(data, status, xhr){
-                _notifyHandler(status, data, xhr);
+                _notify(NOTIFY_GENERIC, status, data, xhr);
                 dfr.resolve(status, data, xhr);
             }).fail(function(xhr, status, error){
-                _notifyHandler(status, error, xhr);
+                _notify(NOTIFY_GENERIC, status, error, xhr);
                 dfr.reject(status, error, xhr);
             });
             return dfr.promise();
 		}
-			
+
         function login(login, password) {
-            var dfr = $.Deferred();
-            _net_call(config.syncserver+'/clientlogin', {login:login, password:password, rememberme: 1}
-            ).done(function(status, data, xhr) {
-                dfr.resolve(data, xhr.responseText);
-            }).fail(function(status, error, xhr) {
-                dfr.reject(error, xhr.responseText);
-            });
-            return dfr.promise();
+            return _net_call(config.syncserver+'/clientlogin', {login:login, password:password, rememberme: 1});
         }
-				
+
+        /*
         function isLoggedIn() {
             return _getCookie(SESSION_COOKIE) ? true : false;
         }
@@ -86,15 +276,17 @@
         function logout() {
             _deleteCookie(SESSION_COOKIE);
         }
+        */
 
 		return {
 			api: {
                 events: {
-                    NOTIFY_GENERIC: NOTIFY_GENERIC
+                    NOTIFY_GENERIC: NOTIFY_GENERIC,
+                    NOTIFY_CLIENT_CREATED: NOTIFY_CLIENT_CREATED
                 },
                 login: login,
-                isLoggedIn: isLoggedIn,
-                logout: logout
+                engine: engine,
+                storage: storage
 			},
 			rhoconfig: config
 		}
--- spec/javascripts/RhoSyncSpec.js
+++ spec/javascripts/RhoSyncSpec.js
@@ -31,9 +31,8 @@ describe("RhoSync", function() {
 
         waitsFor(function(){ return 0 < hdlr.callCount;}, 3000);
         runs(function(){
-            expect(hdlr).toHaveBeenCalledWith("success", "");
+            expect(hdlr).toHaveBeenCalledWith("success", null);
             expect(notified).toBeTruthy();
-
             $(window).unbind(rhosync.api.events.NOTIFY_GENERIC, notify);
         });
     });
@@ -53,10 +52,95 @@ describe("RhoSync", function() {
 
         waitsFor(function(){ return 0 < hdlr.callCount;}, 3000);
         runs(function(){
-            expect(hdlr).toHaveBeenCalledWith("error", "");
+            expect(hdlr).toHaveBeenCalledWith("error", "Unauthorized");
             expect(notified).toBeTruthy();
-
             $(window).unbind(rhosync.api.events.NOTIFY_GENERIC, notify);
         });
     });
+
+    it("should be able to start sync engine", function() {
+        $(window).bind(rhosync.api.events.NOTIFY_CLIENT_CREATED, null, notify);
+
+        rhosync.api.login("lars", "larspass").done(function(){
+            rhosync.api.engine.clientCreate();
+        });
+
+        waitsFor(function(){ return notified;}, 3000);
+        runs(function(){
+            expect(notified).toBeTruthy();
+            $(window).unbind(rhosync.api.events.NOTIFY_CLIENT_CREATED, notify);
+        });
+    });
+
+
+    describe("Rhomobile.db.DbStorage", function() {
+
+        beforeEach(function() {
+            rhosync = RhoSync({syncserver:syncUrl});
+        });
+
+        it("is able to open database", function() {
+            var okHdlr = jasmine.createSpy('for ok');
+            var errHdlr = jasmine.createSpy('for errors');
+
+            expect(rhosync.api.storage.open).toBeDefined();
+            rhosync.api.storage.open().done(okHdlr).fail(errHdlr);
+
+            waitsFor(function(){ return 0 < okHdlr.callCount;}, 3000);
+            runs(function(){
+                expect(errHdlr).not.toHaveBeenCalled();
+            });
+        });
+
+        it("is able to perform a query", function() {
+            var okHdlr = jasmine.createSpy('for ok');
+            var errHdlr = jasmine.createSpy('for errors');
+
+            expect(rhosync.api.storage.executeSQL).toBeDefined();
+            rhosync.api.storage.executeSQL("SELECT name FROM sqlite_master WHERE type='table'", null).done(function(){
+                okHdlr(arguments);
+            }).fail(function(){
+                errHdlr(arguments);
+            });
+
+            waitsFor(function(){ return 0 < okHdlr.callCount;}, 3000);
+            runs(function(){
+                expect(errHdlr).not.toHaveBeenCalled();
+            });
+        });
+
+        it("is able to initialized", function() {
+            var okHdlr = jasmine.createSpy('for ok');
+            var errHdlr = jasmine.createSpy('for errors');
+            expect(rhosync.api.storage.initSchema).toBeDefined();
+            expect(rhosync.api.storage.executeSQL).toBeDefined();
+            rhosync.api.storage.initSchema().done(okHdlr).fail(errHdlr);
+
+            waitsFor(function(){ return 0 < okHdlr.callCount;}, 3000);
+            runs(function(){
+                expect(errHdlr).not.toHaveBeenCalled();
+            });
+
+            var names;
+            rhosync.api.storage.getAllTableNames().done(function(tx, tbNames){
+                okHdlr(arguments);
+                names = tbNames;
+            }).fail(function(){
+                errHdlr(arguments);
+            });
+
+            waitsFor(function(){ return 1 < okHdlr.callCount;}, 3000);
+            runs(function(){
+                expect(errHdlr).not.toHaveBeenCalled();
+                expect(names).toBeDefined();
+                expect(names.length).toEqual(4+1);
+                expect(names).toContain('sources');
+            });
+
+        });
+
+    });
+
+
+
 });
