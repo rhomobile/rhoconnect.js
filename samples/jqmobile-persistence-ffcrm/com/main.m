7f6527efdd35f33fa563ef806316241881c78b3d
lucas campbell-rossen <lucas@rhomobile.com>
2011-07-14 13:36:18 -0700

    xcode proj to new sample

---
+++ samples/jqmobile-persistence-ffcrm/com/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  com
+//
+//  Created by lucas campbell-rossen on 6/21/11.
+//  Copyright none 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
